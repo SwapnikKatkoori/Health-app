cbaa66f6aa7d747b6261a7efc6c3b029f2a48398
Swapnik Katkoori <katkoor2@msu.edu>
2019-02-27 16:34:28 -0500

    Added the initial sign in page

---
+++ Health App/RegisterViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RegisterViewController.swift
+//  Health App
+//
+//  Created by Swapnik R. Katkoori on 2/27/19.
+//  Copyright © 2019 Swapnik R. Katkoori. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
--- Health App/ViewController.swift
+++ Health App/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    //To do: Need to create authentication system with FireBase
+    @IBAction func signInButton(_ sender: UIButton) {
+        print("pressed")
+        performSegue(withIdentifier: "signIn", sender:self)
+        
+        
+    }
+    
+    //Should segue to the Register Page
+    @IBAction func registerButton(_ sender: UIButton) {
+        print("register pressed")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    
 
 }
 
