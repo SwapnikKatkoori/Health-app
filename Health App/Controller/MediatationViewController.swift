bfc9f4793ccceb726012ecc1e928022de6a25097
Prahadeeswaran Dharmalingam <Prahadeeswaran@Prahadeeswaran-Dharmalingams-MacBook-Pro.local>
2019-03-05 01:14:43 -0600

    Timerchanges

--- Health App/Controller/MediatationViewController.swift
+++ Health App/Controller/MediatationViewController.swift
@@ -10,6 +10,36 @@ import UIKit
 
 class MeditationViewController: UIViewController{
     
+    
+    @IBOutlet weak var Label: UILabel!
+    
+    var time = 0
+    
+    //timer
+    var timer = Timer( )
+    
+    
+    @IBAction func Start(_ sender: UIButton)
+    {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(MeditationViewController.action), userInfo: nil, repeats: true)
+    }
+    @IBAction func Pause(_ sender: UIButton)
+    {
+        timer.invalidate()
+    }
+    
+    @IBAction func Reset(_ sender: UIButton)
+    {
+        //timer.invalidate()
+        time = 0
+        label.text = ("0")
+    }
+    func action()
+    {
+        time += 1
+        label.text = string(time)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
